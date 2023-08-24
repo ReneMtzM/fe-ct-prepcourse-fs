555d70fd18a27ae1edea713a98d5789055a55955
ReneMtzM <rene.mtz.m@gmail.com>
2023-08-23 19:00:56 -0600

    Ejercicios 01 y 02 - Modulo 7

--- M07 JavaScript Clases/Ejercicio 01.js
+++ M07 JavaScript Clases/Ejercicio 01.js
@@ -10,18 +10,53 @@ function crearUsuario() {
    // El valor {{nombre}} debe ser el nombre definido para la instancia de la clase.
    // Retornar la clase.
    // Tu código:
+
+   class Usuario {
+      constructor (usuario, nombre, email, password){
+         this.usuario = usuario;
+         this.nombre = nombre;
+         this.email = email;
+         this.password = password;
+      }
+
+      saludar(){
+         return ('Hola, mi nombre es ' + this.nombre);
+      }
+
+   }
+   
+   var primerUsuario = new Usuario('ReneMtz', 'René', 'algun@mail.com','12345');
+   return (Usuario);
 }
 
 function agregarMetodoPrototype(Usuario) {
    // Agrega un método al prototipo de "Usuario".
    // El método debe llamarse "saludar" y debe devolver el string "Hello World!".
    // Tu código:
+   Usuario.prototype.saludar = function (){
+      return ('Hello World!');
+   }
 }
 
 function agregarStringInvertida() {
    // Agrega un método al prototipo de "String" que devuelva la misma cadena de caracteres, pero invertida.
    // El método debe llamarse "reverse".
    // [PISTA]: necesitarás utilizar el objeto "this".
+   String.prototype.reverse = function(){
+      //convertir cadena en arreglo
+      var arregloString = this.split('');
+      var arregloInvertido = [];
+      for(var i=0; i<arregloString.length; i++){
+         //Recorrer el arragloString y enviar cada caracter encontrado al inicio del arregloInvertido
+         arregloInvertido.unshift(arregloString[i]);
+      }
+      //convertir el arregloInvertido en string
+      var stringInvertida = arregloInvertido.join('');
+      return(stringInvertida);
+   }
+
+
+
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
--- M07 JavaScript Clases/Ejercicio 02.js
+++ M07 JavaScript Clases/Ejercicio 02.js
@@ -6,6 +6,25 @@ class Persona {
    // Debe tener las propiedades: "nombre", "apellido", "edad" y "domicilio".
    // Debe tener un método llamado "detalle" que nos devuelva un objeto con las propiedades de la persona y
    // sus valores.
+   constructor(nombre, apellido, edad, domicilio){
+      this.nombre = nombre;
+      this.apellido = apellido;
+      this.edad = edad;
+      this.domicilio = domicilio;
+   }
+
+   detalle (){
+      var persona = { 
+         nombre:this.nombre, 
+         apellido:this.apellido, 
+         edad:this.edad, 
+         domicilio:this.domicilio
+      };
+
+      return (persona);
+   }
+
+
 }
 
 function crearInstanciaPersona(nombre, apellido, edad, domicilio) {
@@ -13,6 +32,8 @@ function crearInstanciaPersona(nombre, apellido, edad, domicilio) {
    // Recibirás las propiedades por parámetro.
    // Retornar la instancia creada.
    // Tu código:
+   primerPersona = new Persona(nombre, apellido, edad, domicilio);
+   return (primerPersona);
 }
 
 function agregarMetodo() {
@@ -20,6 +41,11 @@ function agregarMetodo() {
    // Este método toma la propiedad "nombre" y "edad", y devuelve el string:
    // Ejemplo: "Juan, 22 años".
    // Tu código:
+
+   Persona.prototype.datos = function () {
+      return (this.nombre + ', ' + this.edad + ' años');
+   }
+
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
